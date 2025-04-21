da54fd3c10dd82136632545a03e9f144ad28be5c
Universaltools693 <universaltools693@gmail.com>
2025-04-21 11:16:28 +0530

    Delete ai-code-snippet-generator directory

--- ai-code-snippet-generator/src/components/AdPlaceholder.jsx
+++
@@ -1,9 +0,0 @@
-function AdPlaceholder() {
-  return (
-    <div className="ad-placeholder" style={{ height: '90px', lineHeight: '90px' }}>
-      Ad Space
-    </div>
-  );
-}
-
-export default AdPlaceholder;
