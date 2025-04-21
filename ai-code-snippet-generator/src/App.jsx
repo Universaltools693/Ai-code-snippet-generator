0ff31708c7d0599e7b7a87f6f20364c8272c05b1
Universaltools693 <universaltools693@gmail.com>
2025-04-21 10:41:01 +0530

    Add files via upload
    Added

---
+++ ai-code-snippet-generator/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Terms from './pages/Terms';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
